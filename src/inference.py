fff3eac50934ff86e7647c29673e76867339348f
Egor Svinarev <egor-rdd@mail.ru>
2026-02-09 23:44:07 +0300

    кэширование загружаемой модели между запусками приложения

--- src/inference.py
+++ src/inference.py
@@ -1,10 +1,17 @@
 import tensorflow_hub as hub
 import tensorflow as tf
+import streamlit as st
 
-yamnet = hub.load("https://tfhub.dev/google/yamnet/1")
-class_map = yamnet.class_map_path().numpy().decode("utf-8")
+@st.cache_resource
+def load_model():
+    """Загружает модель YAMNet с кэшированием"""
+    return hub.load("https://tfhub.dev/google/yamnet/1")
 
-def _load_classes():
+@st.cache_data
+def load_class_names():
+    """Загружает названия классов с кэшированием"""
+    yamnet = load_model()
+    class_map = yamnet.class_map_path().numpy().decode("utf-8")
     classes = []
     with open(class_map) as f:
         next(f)
@@ -12,8 +19,8 @@ def _load_classes():
             classes.append(line.split(",")[2])
     return classes
 
-CLASS_NAMES = _load_classes()
-
 def run_inference(audio):
+    """Выполняет инференс на аудио"""
+    yamnet = load_model()
     scores, _, _ = yamnet(audio)
     return tf.reduce_mean(scores, axis=0).numpy()
